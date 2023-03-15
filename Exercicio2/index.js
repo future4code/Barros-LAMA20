716c913898e47b80cdc393ecb1fa3497bf90458b
jose <josemorcelli1@gmail.com>
2023-03-14 21:29:24 -0300

    Revisao Backend

--- Exercicio2/index.js
+++
@@ -1,3 +0,0 @@
-/*------------------------------------- Exercício 2
-Crie uma aplicação Node que recebe uma string representando uma operação matemática e dois valores numéricos.
-O retorno deverá ser o resultado da operação selecionada utilizando os 2 valores fornecidos.*/
